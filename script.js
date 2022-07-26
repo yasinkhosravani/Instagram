97a482710f02aa6589aa53d1cab763b51a9b8a51
yasin khosravani <31961300+yasinkhosravani@users.noreply.github.com>
2022-07-26 13:25:00 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const titleValue = document.getElementById("title");
 const authorValue =  document.getElementById("author");
 const descriptionValue =  document.getElementById("comment");
-const uploudValue = document.getElementById("exampleFormControlFile1");
+const uploudValue = document.getElementById("profile_pic");
 const submitValue = document.getElementById("btn");
 const div =document.getElementById("item");
 
@@ -33,10 +33,12 @@ const div =document.getElementById("item");
 
     }
     //Now Weblog Design
+    console.log(nowComment.uploud);
     
     const d1Item = document.createElement("div")
     const d2Item = document.createElement("div")
-    const img1Item = document.createElement("img")
+    const i1Item = document.createElement("i")
+    const i2Item = document.createElement("i")
     const p1Item = document.createElement("p")
         p1Item.innerHTML = nowComment.author
     const img2Item = document.createElement("img")
@@ -50,8 +52,9 @@ const div =document.getElementById("item");
     //appendChild Item
 
 
-    d1Item.appendChild(img1Item);d1Item.appendChild(p1Item)
-    d2Item.appendChild(h5Item);d2Item.appendChild(p2Item);d2Item.appendChild(btnItem)
+    d1Item.appendChild(i1Item);d1Item.appendChild(p1Item)
+    h5Item.appendChild(btnItem);btnItem.appendChild(i2Item)
+    d2Item.appendChild(h5Item);d2Item.appendChild(p2Item);
     div.appendChild(d1Item);div.appendChild(d2Item);div.appendChild(img2Item)
     
 
@@ -63,7 +66,7 @@ const div =document.getElementById("item");
 
 
 
-
+        img2Item.setAttribute("id","uploud")
         // bt3Item.setAttribute("class","action"+i);
         // divItem.appendChild(d1Item).setAttribute("class","action"+i);
         // divItem.appendChild(d1Item).setAttribute("id",i);
@@ -74,12 +77,16 @@ const div =document.getElementById("item");
     
 
     // classlist
-    d1Item.classList.add("card"," mx-auto")
-    d2Item.classList.add("card-body")
-    img2Item.classList.add("card-img-top")
+    d1Item.classList.add("card-body","d-flex","ms-5")
+    d2Item.classList.add("card-body","ms-5")
+    i1Item.classList.add("bi","bi-person-circle","h1")
+    i2Item.classList.add("bi","bi-heart")
+    p1Item.classList.add("h3","ms-3","my-auto")
+    img2Item.classList.add("card-img-top","bg-danger","w-75","mx-auto")
     h5Item.classList.add("card-title")
     p2Item.classList.add("card-text")
-    btnItem.classList.add("btn btn-primary")
+    btnItem.classList.add("border-0","bg-white")
+
 
 
    
